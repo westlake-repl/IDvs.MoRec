2855d25da905915f6c9016b08d10232674783f6e
zyuanat <94222506+zyuanat@users.noreply.github.com>
2023-08-01 15:33:13 +0800

    Delete bce_text/main-2stage directory

--- bce_text/main-2stage/data_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .utils import *
-from .preprocess import read_news, read_news_bert, get_doc_input_bert, read_behaviors
-from .dataset import BuildTrainDataset, BuildEvalDataset, SequentialDistributedSampler
-from .metrics import eval_model, get_item_embeddings, get_item_word_embs, get_user_embeddings
-
--- bce_text/main-2stage/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import Model, Bert_Encoder
