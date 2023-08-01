74a0d912624a25f7c3eff6460761cbf82c8dbb0d
zyuanat <94222506+zyuanat@users.noreply.github.com>
2023-08-01 15:30:19 +0800

    Delete bce_vision/main-2stage directory

--- bce_vision/main-2stage/data_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .utils import *
-from .preprocess import read_images, read_behaviors
-from .special import read_behaviors_special, eval_model_special
-from .dataset import LMDB_Image, BuildTrainDataset, BuildEvalDataset, \
-    Build_Lmdb_Eval_Dataset, Build_Id_Eval_Dataset, SequentialDistributedSampler
-from .metrics import eval_model, get_user_embeddings, get_item_embeddings, get_image_embs
-
--- bce_vision/main-2stage/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import Model, Resnet_Encoder, Vit_Encoder
